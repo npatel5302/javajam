9dcaf762a850188b277150741971b9c808c8a83b
neel2911 <neelrokr@gmail.com>
2022-12-03 19:03:35 -0500

    add page not found page

--- src/pages/nomatch/Nomatch.js
+++ src/pages/nomatch/Nomatch.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./Nomatch.css";
 
 const Nomatch = () => {
-  return <div>Nomatch</div>;
+  return (
+    <div className="no-match-wrapper">
+      <h1>404</h1>
+      <p>Page not found</p>
+    </div>
+  );
 };
 
 export default Nomatch;
